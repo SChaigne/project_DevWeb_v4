0e5f0fc3b74170803a46d6b4171d797f21160a64
sChaigne <stephane.chaigne@gmail.com>
2022-05-12 12:15:18 +0200

    opkok

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -94,9 +94,8 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_accueil'));
+        throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
     protected function getLoginUrl()
