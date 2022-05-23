c90c92cc8b42532fe87d5950245d768d2ad9360c
Elouan PRIME <elouan.prime@etu.univ-nantes.fr>
2022-05-23 12:08:47 +0200

    page abonnement

---
+++ src/Controller/SubscribesOfUserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SubscribesOfUserController extends AbstractController
+{
+    /**
+     * @Route("/abonnements", name="app_subscribes_of_user")
+     */
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        if(!$user){
+            return $this->redirectToRoute('app_login');
+        }
+
+        $subUser = $user->getSubscribes();
+        $cryptoUser = array();
+        foreach($subUser as $sub){
+            foreach($sub->getIdCrypto() as $cryptoSub){
+                array_push($cryptoUser,$cryptoSub);
+            }
+            
+        }
+        //dd($cryptoUser);
+
+        return $this->render('subscribes_of_user/index.html.twig', [
+            'cryptos' => $cryptoUser,
+        ]);
+    }
+}
