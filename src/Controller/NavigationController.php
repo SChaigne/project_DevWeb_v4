db426df3e645e54384eca0691730cf9a714a938e
Elouan PRIME <elouan.prime@etu.univ-nantes.fr>
2022-05-29 21:28:33 +0200

    admin

--- src/Controller/NavigationController.php
+++ src/Controller/NavigationController.php
@@ -7,18 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-class NavigationController extends AbstractController // https://www.univ-orleans.fr/iut-orleans/informatique/intra/tuto/php/symfony-securitybundle-auth.html
+class NavigationController extends AbstractController
 {
-    /**
-     * @Route("{_locale}/navigation", name="app_navigation")
-     */
-    public function index(): Response
-    {
-        return $this->render('navigation/index.html.twig', [
-            'controller_name' => 'NavigationController',
-        ]);
-    }
-
     /**
      * @Route("/", name="home")
      */
@@ -29,17 +19,6 @@ class NavigationController extends AbstractController // https://www.univ-orlean
         return $this->redirectToRoute('app_accueil');
     }
 
-    // /**
-    //  * Nécessite juste d'être connecté
-    //  * @Route("/membre", name="membre")
-    //  * @IsGranted("IS_AUTHENTICATED_FULLY")
-    //  * fonctionne aussi avec ROLE_USER
-    //  */
-    public function membre()
-    {
-        return $this->render('navigation/membre.html.twig');
-    }
-
 
     /**
      * Besoin des droits admin
@@ -47,18 +26,8 @@ class NavigationController extends AbstractController // https://www.univ-orlean
      */
     public function admin()
     {
-        // * @IsGranted("ROLE_ADMIN")
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        //récupération de l'utilisateur security>Bundle // A mettre au dessus
-        $utilisateur = $this->getUser();
-
-        //vérification des droits.
-        // if ($utilisateur && in_array('ROLE_ADMIN', $utilisateur->getRoles())) {//TODO a decommenter une fois le menu admin fini
         return $this->render('navigation/admin.html.twig');
-        // }//TODO a decommenter une fois le menu admin fini
-
-        //redirection
-        // $session->set("message", "Vous n'avez pas le droit d'acceder à la page admin vous avez été redirigé sur cette page");
-        // return $this->redirectToRoute('home'); //TODO a decommenter une fois le menu admin fini
     }
 }
